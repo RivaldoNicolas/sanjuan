062c215d574e91caf1156050d3c3d29cca414fa5
Rivaldo Nicolas <rivaldonicolasfabian@gmail.com>
2024-12-13 18:34:53 -0500

    nuevos cambios

--- elBorrachoFiel/settings.py
+++ elBorrachoFiel/settings.py
@@ -87,7 +87,7 @@ WSGI_APPLICATION = 'elBorrachoFiel.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         # Replace this value with your local database's connection string.
-        default='postgresql://postgres:postgres@localhost:5432/el-borracho-fiel',
+        default='postgresql://postgres:postgres@localhost:5432/sanjuan',
         conn_max_age=600
     )
 }
